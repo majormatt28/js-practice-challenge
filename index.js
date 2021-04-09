bf38c416ba2a81ce0eadc425251c318046837288
Matthew <elijah2895@hotmail.com>
2021-04-09 19:47:56 -0400

    done first deliverable

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
-
 // write code here
+function travelerObj () {
+fetch ('http://localhost:3000/travelers/1')
+    .then(response => response.json())
+    .then(travelerArr => {
+        const profile = document.querySelector("div#profile")
+
+        const profileImg = profile.querySelector("img")
+        profileImg.src = travelerArr.photo
+        profileImg.alt = travelerArr.name
+
+        const profileH2 = document.querySelector("h2")
+        profileH2.textContent = travelerArr.name
+
+        const profileEm = document.querySelector("em")
+        profileEm.textContent = travelerArr.nickname
+
+        const likes = document.querySelector(".likes")
+        likes.textContent = `${travelerArr.likes} Likes`
+    })
+} 
+travelerObj()
+   
