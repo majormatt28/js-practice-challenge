81a947f579a9bf65679674d8977dce98fabbf364
Matthew <elijah2895@hotmail.com>
2021-04-09 19:49:07 -0400

    did somme cleanup

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 // write code here
 function travelerObj () {
-fetch ('http://localhost:3000/travelers/1')
-    .then(response => response.json())
-    .then(travelerArr => {
-        const profile = document.querySelector("div#profile")
+    fetch ('http://localhost:3000/travelers/1')
+        .then(response => response.json())
+        .then(travelerArr => {
+            const profile = document.querySelector("div#profile")
 
-        const profileImg = profile.querySelector("img")
-        profileImg.src = travelerArr.photo
-        profileImg.alt = travelerArr.name
+            const profileImg = profile.querySelector("img")
+            profileImg.src = travelerArr.photo
+            profileImg.alt = travelerArr.name
 
-        const profileH2 = document.querySelector("h2")
-        profileH2.textContent = travelerArr.name
+            const profileH2 = document.querySelector("h2")
+            profileH2.textContent = travelerArr.name
 
-        const profileEm = document.querySelector("em")
-        profileEm.textContent = travelerArr.nickname
+            const profileEm = document.querySelector("em")
+            profileEm.textContent = travelerArr.nickname
 
-        const likes = document.querySelector(".likes")
-        likes.textContent = `${travelerArr.likes} Likes`
-    })
+            const likes = document.querySelector(".likes")
+            ikes.textContent = `${travelerArr.likes} Likes`
+        })
 } 
 travelerObj()
+
+
    
